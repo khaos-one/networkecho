35054f207ae68da3cde44c0e900009ac956b99ff
Егор Зеленский <i@khaos.su>
2015-09-10 20:00:43 +0300

    Initial commit

---
+++ UdpEcho.Client/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UdpEcho.Client
+{
+    internal class Program
+    {
+        internal static void Usage(int exitCode = 0)
+        {
+            Console.WriteLine("Usage: UdpEcho.Client <hostname> <port>");
+            Console.WriteLine("Utility that send arbitary strings to");
+            Console.WriteLine("and receives them back over UDP/IP.");
+            Console.WriteLine();
+            Environment.Exit(exitCode);
+        }
+
+        internal static void Main(string[] args)
+        {
+            if (args.Length < 2)
+                Usage(-1);
+
+            var hostname = args[0];
+            int port = 0;
+
+            if (!int.TryParse(args[1], out port))
+                Usage(-2);
+
+            var udp = new UdpClient();
+
+            while (true)
+            {
+                IPEndPoint remote = null;
+
+                try
+                {
+                    Console.Write("< ");
+                    var input = Console.ReadLine();
+                    var packet = Encoding.UTF8.GetBytes(input);
+                    udp.Send(packet, packet.Length, hostname, port);
+                    var received = udp.Receive(ref remote);
+                    var output = Encoding.UTF8.GetString(received);
+                    Console.WriteLine($"> {output}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"! Exception: {e}.");
+                }
+            }
+        }
+    }
+}
---
+++ UdpEcho.Server/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UdpEcho
+{
+    internal class Program
+    {
+        internal static void Usage(int exitCode = 0)
+        {
+            Console.WriteLine("Usage: UdpEcho.Server.exe <port_number>");
+            Console.WriteLine("Establishes an UDP/IP echo server.");
+            Console.WriteLine();
+            Environment.Exit(exitCode);
+        }
+
+        internal static void Main(string[] args)
+        {
+            if (args.Length < 1)
+                Usage(1);
+
+            int port = 0;
+
+            if (!int.TryParse(args[0], out port))
+                Usage(2);
+
+            var udp = new UdpClient(port);
+            
+            while (true)
+            {
+                IPEndPoint remote = null;
+
+                try
+                {
+                    var packet = udp.Receive(ref remote);
+                    udp.Send(packet, packet.Length, remote);
+                    Console.WriteLine($"-> Packet of {packet.Length} bytes received from {remote.Address.ToString()}.");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Exception: {e}.");
+                }
+            }
+        }
+    }
+}
